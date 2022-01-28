5fab5d09aaa081b8d98f2f5d23a54e88f1a60305
XuEthan <exu2014sd@gmail.com>
2022-01-27 19:18:40 -0800

    added edge_case1.md; edited MarkdownParse.java
    Edited MarkdownParse.java to resolve edge case issue

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,25 +5,30 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-    public static ArrayList<String> getLinks(String markdown) {
+    public static ArrayList<String> getLinks(String[] markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
+
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+        for(int i = 0; i<markdown.length; i++){
+            //if the current line contains an open bracket, then search for the link
+            if(markdown[i].contains("[")){
+                // find the next [, then find the ], then find the (, then take up to
+                // the next )
+                int nextOpenBracket = markdown[i].indexOf("[", currentIndex);
+                int nextCloseBracket = markdown[i].indexOf("]", nextOpenBracket);
+                int openParen = markdown[i].indexOf("(", nextCloseBracket);
+                int closeParen = markdown[i].indexOf(")", openParen);
+                toReturn.add(markdown[i].substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
+        String[] inp = contents.split(System.lineSeparator());
+        ArrayList<String> links = getLinks(inp);
         System.out.println(links);
     }
 }
